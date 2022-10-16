abcb188771b863a64fcc7fc8cb4853f5759410d8
wjohnson-softwarescientists <walter@softwarescientists.com>
2022-10-16 01:41:27 -0400

    action

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ provider "aws" {
 }
 variable "walter" {
   type = string
-  default = "Hey There. I did it. Atlantis Unlocked! WBJ JOHNSON"
+  default = "Hey There. I did it. Atlantis Unlocked! WBJ JOHNSON GO FIGURE"
 }
 
 output "me" {
